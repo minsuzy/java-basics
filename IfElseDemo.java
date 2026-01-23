212402c076cae64ecb11650cdfea2cc8d45f3d8a
Karthika <kk9902185@gmail.com>
2026-01-23 21:57:23 +0530

    Add IfElseDemo class to check number sign

---
+++ IfElseDemo.java
@@ -0,0 +1,13 @@
+public class IfElseDemo {
+    public static void main(String[] args) {
+        int number = 10;
+
+        if (number > 0) {
+            System.out.println("Number is positive");
+        } else if (number < 0) {
+            System.out.println("Number is negative");
+        } else {
+            System.out.println("Number is zero");
+        }
+    }
+}

902fa68ae7ffa94bb96f6634a10b5a8e1c407138
Karthika <kk9902185@gmail.com>
2026-01-23 21:56:36 +0530

    Create VariablesDemo.java

---
+++ VariablesDemo.java
@@ -0,0 +1,13 @@
+public class VariablesDemo {
+    public static void main(String[] args) {
+        int age = 22;
+        double marks = 85.5;
+        char grade = 'A';
+        boolean passed = true;
+
+        System.out.println("Age: " + age);
+        System.out.println("Marks: " + marks);
+        System.out.println("Grade: " + grade);
+        System.out.println("Passed: " + passed);
+    }
+}

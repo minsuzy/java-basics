6092a2be8c4154accba620d148dfcbd19c992f28
Karthika K <kk9902185@gmail.com>
2026-01-24 18:54:48 +0530

    Add StringDemo class to demonstrate string methods

---
+++ StringDemo.java
@@ -0,0 +1,13 @@
+public class StringDemo {
+    public static void main(String[] args) {
+        String name = "Karthika";
+        String college = "College";
+
+        System.out.println("Name: " + name);
+        System.out.println("College: " + college);
+
+        System.out.println("Length of name: " + name.length());
+        System.out.println("Uppercase name: " + name.toUpperCase());
+        System.out.println("Character at index 2: " + name.charAt(2));
+    }
+}

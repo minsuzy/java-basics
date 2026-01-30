691bc910ee7f9f1cf8d6786d9e5e82edc9149dba
Karthika K <karthikak.careerhub@gmail.com>
2026-01-30 22:46:20 +0530

    Add Student class and demo in ClassObjectDemo

---
+++ ClassObjectDemo.java
@@ -0,0 +1,19 @@
+class Student {
+    String name;
+    int age;
+
+    void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
+
+public class ClassObjectDemo {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.name = "Karthika";
+        s1.age = 22;
+
+        s1.display();
+    }
+}
